f62be15c9ec218a27fde70a74f754d83cbda6eba
SerKa22 <104494413+SerKa22@users.noreply.github.com>
2022-09-24 10:01:45 +0300

    rjv

---
+++ calkulator/calkulator/Calkulator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace calkulator
+{
+    class Calkulator
+    {
+        for(i = 0; i < input.Length; i++){
+            if (stroka1 == "+" || "-"){
+            l=0;
+            for(j;j<k;j++){
+                chislo[l]=prim[j];
+                l++;
+            }
+        
+            }
+        
+            else{
+            k=i;
+            }
+
+    }
+}
---
+++ calkulator/calkulator/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace calkulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Hello World!");
+            for(i=0; i < input.Length; i++)
+            {
+                if (stroka1 == "+" || "-")
+                {
+                    j = i;
+
+                }
+            }
+        
+        }
+    }
+}

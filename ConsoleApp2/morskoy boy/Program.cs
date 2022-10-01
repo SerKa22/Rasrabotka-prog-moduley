2868f709bb0c0f8eaf99ef0eecd1665e296f1f06
SerKa22 <ant1393@gmail.com>
2022-10-01 10:47:20 +0300

    Морсой бой V.1

---
+++ ConsoleApp2/morskoy boy/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace morskoy_boy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
